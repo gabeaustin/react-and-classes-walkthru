2442dc9592f672b0d49077c8a9cfdb66f1efd4ef
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-03 11:44:46 -0600

    changed const to class in App.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text-center mt-5">Let's Get Classy!!</h1>
+                <h4 className="text-center mt-1">You like that pun there...hehehehe!</h4>
+            </div>
+        );
+    };
+};
+
+export default App;
